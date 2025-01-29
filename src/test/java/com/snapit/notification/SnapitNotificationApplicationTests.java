d7dfc501a869651151745b4e6ac977e7aabeb3b9
GuiRodriguero <gui.rodriguero@hotmail.com>
2025-01-28 21:47:11 -0300

    Creating tests

--- src/test/java/com/snapit/notification/SnapitNotificationApplicationTests.java
+++ src/test/java/com/snapit/notification/SnapitNotificationApplicationTests.java
@@ -2,12 +2,15 @@ package com.snapit.notification;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SnapitNotificationApplicationTests {
 
 	@Test
 	void contextLoads() {
+        //not to do
 	}
 
 }
